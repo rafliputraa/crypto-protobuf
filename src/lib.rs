72937b8093328b7613f4c0c6366de9ba043abad6
rafliputraa <rafli.putra712@gmail.com>
2024-07-07 00:01:14 +0700

    change the expose to the upper level

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,5 @@ pub mod crypto {
     pub mod watchlist {
         include!(concat!(env!("CARGO_MANIFEST_DIR"), "/protobuf/crypto_watchlist.rs"));
     }
-
-    pub use watchlist::*;
 }
+pub use crypto::watchlist::*;
